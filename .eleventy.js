aab9c2f61a253b3b7cc78c37a653a2c2170aeb4c
Dan Brown <dbbnrl@gmail.com>
2022-12-19 11:03:50 -0500

    Got sidenotes working.

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,15 @@
 const yaml = require("js-yaml");
 const { DateTime } = require("luxon");
 const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
+const pluginNavigation = require("@11ty/eleventy-navigation");
+const pluginMermaid = require("@kevingimbel/eleventy-plugin-mermaid");
 const htmlmin = require("html-minifier");
+
+const markdownIt = require("markdown-it");
+const markdownItPandoc = require("markdown-it-pandoc");
+const markdownItAnchor = require("markdown-it-anchor");
+const markdownItTOC = require("markdown-it-toc-done-right");
+const markdownItSidenote = require("./sidenotes.js");
 
 module.exports = function (eleventyConfig) {
   // Disable automatic use of your .gitignore
@@ -17,8 +25,35 @@ module.exports = function (eleventyConfig) {
     );
   });
 
+  // Markdown-it plugins
+  let md = markdownIt({html: true, typographer: true})
+    .use(markdownItPandoc, { footnotes: false })
+    .use(markdownItSidenote)
+    .use(markdownItAnchor)
+    .use(markdownItTOC);
+  eleventyConfig.setLibrary("md", md);
+
   // Syntax Highlighting for Code blocks
   eleventyConfig.addPlugin(syntaxHighlight);
+
+  // Navigation
+  eleventyConfig.addPlugin(pluginNavigation);
+
+  /*
+  // Mermaid diagrams
+  const highlighter = eleventyConfig.markdownHighlighter;
+  eleventyConfig.addMarkdownHighlighter((str, language) => {
+    if (language === "mermaid") {
+      return `<pre class="mermaid">${str}</pre>`;
+    }
+    if (highlighter) {
+      return highlighter(str, language);
+    }
+  });
+  */
+
+  // Mermaid plugin
+  eleventyConfig.addPlugin(pluginMermaid);
 
   // To Support .yaml Extension in _data
   // You may remove this if you can use JSON
@@ -30,6 +65,7 @@ module.exports = function (eleventyConfig) {
     "./node_modules/alpinejs/dist/cdn.min.js": "./static/js/alpine.js",
     "./node_modules/prismjs/themes/prism-tomorrow.css":
       "./static/css/prism-tomorrow.css",
+    //"./src/static/js/sidenotes.js": "./static/js/sidenotes.js",
   });
 
   // Copy Image Folder to /_site
@@ -38,6 +74,7 @@ module.exports = function (eleventyConfig) {
   // Copy favicon to route of /_site
   eleventyConfig.addPassthroughCopy("./src/favicon.ico");
 
+  /*
   // Minify HTML
   eleventyConfig.addTransform("htmlmin", function (content, outputPath) {
     // Eleventy 1.0+: use this.inputPath and this.outputPath instead
@@ -52,6 +89,7 @@ module.exports = function (eleventyConfig) {
 
     return content;
   });
+  */
 
   // Let Eleventy transform HTML files as nunjucks
   // So that we can use .html instead of .njk
